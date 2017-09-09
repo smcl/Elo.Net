cfb1081f1ad7c9ce067166402bc346f50367c8f2
smcl <sean.mclemon@gmail.com>
2017-09-09 15:42:26 +0200

    initial code commit

---
+++ Library.fs
@@ -0,0 +1,89 @@
+namespace Elo
+
+open System
+open System.Collections.Generic
+
+type BasicResult =
+    member this.Win = 1.0
+    member this.Draw = 0.5
+    member this.Loss = 0.0
+
+type Competitor(competitorId:string, rating:double) = 
+    let mutable _rating = rating
+    member this.CompetitorId = competitorId
+    member this.Rating 
+        with get() = _rating
+        and set(value:double) = _rating <- value
+
+    member this.TransformedRating() = 
+        10.0 ** (this.Rating / 400.0)
+
+    member private this.CalculateExpected(opponent:Competitor) =
+        this.TransformedRating() / (this.TransformedRating() + opponent.TransformedRating())
+
+    member this.CalculatePoints(opponent:Competitor, result:double, k:double) =
+        k * (result - this.CalculateExpected(opponent))
+
+    override this.ToString() = 
+        String.Format("{0}: {1}", this.CompetitorId, this.Rating)
+        
+
+type Result(competitorId1:string, competitorId2:string, result:double) =
+    member this.CompetitorId1 = competitorId1
+
+    member this.CompetitorId2 = competitorId2
+
+    member this.Result = result
+
+
+type EloSettings (k:double, initialRating:double) =
+    member this.K = k
+    member this.DefaultInitialRating = initialRating
+
+
+type EloSystem(settings:EloSettings) =
+    let mutable _competitors = new Dictionary<string, Competitor>()
+
+    member this.Settings = settings
+
+    member this.Competitors 
+        with get() = _competitors
+
+    member this.AddResults (results:seq<Result>) =
+        // 1. initialise all the competitors
+        let competitorsFromThisRound = 
+            results 
+            |> Seq.collect (fun r -> seq { yield r.CompetitorId1; yield r.CompetitorId2 })
+            |> Seq.distinct
+
+        // 2. accumulate each competitors's rating points from this round of results
+        let accumulatedPoints = new Dictionary<string, double>()
+        for competitorId in competitorsFromThisRound do 
+            accumulatedPoints.Add(competitorId, 0.0)
+        
+        for result in results do   
+            let c1:Competitor = this.GetCompetitor(result.CompetitorId1)
+            let c2:Competitor = this.GetCompetitor(result.CompetitorId2)
+            accumulatedPoints.[result.CompetitorId1] <- 
+                accumulatedPoints.[result.CompetitorId1] + c1.CalculatePoints(c2, result.Result, this.Settings.K) 
+            accumulatedPoints.[result.CompetitorId2] <- 
+                accumulatedPoints.[result.CompetitorId2] + c2.CalculatePoints(c1, 1.0 - result.Result, this.Settings.K) 
+
+        // 3. add the rating points to each competitor
+        for competitorId in accumulatedPoints.Keys do
+            let competitor:Competitor = this.GetCompetitor(competitorId)
+            competitor.Rating <- competitor.Rating + accumulatedPoints.[competitor.CompetitorId]
+
+    member this.Rankings =
+        this.Competitors.Values 
+        |> Seq.sortByDescending (fun c -> c.Rating)
+
+    member private this.GetCompetitor (name: string) =
+        match this.Competitors.TryGetValue name with
+        | true, competitor -> competitor
+        | _                -> this.CreateCompetitor(name)
+
+    member private this.CreateCompetitor (name:string) =
+        let competitor = Competitor(name, this.Settings.DefaultInitialRating)
+        this.Competitors.[name] <- competitor
+        competitor
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Elo.Net
-A simple Elo rating system written in F#
+# Elo
+
+This is a library which implements a simple [Elo rating system](https://en.wikipedia.org/wiki/Elo_rating_system) in F#.
+
+## Installing & Using
+
+Install from [NuGet](https://www.nuget.org/packages/Elo/) via Visual Studio's package manager console
+
+    PM> install-package Elo
+
+... or using the `dotnet` command:
+
+    $ dotnet add package Elo
+
+... and import the `Elo` namespace, initialise an `EloSystem` where each team starts with having 1000 points, with [K=32](https://en.wikipedia.org/wiki/Elo_rating_system#Most_accurate_K-factor) and add results to the system. Below is an example in F# of how we'd initialize a ranking system and populate it with the results from the first six games of the [2017 Confederations Cup](http://www.fifa.com/confederationscup/matches/index.html):
+
+    open Elo;
+
+    let rankings = EloSystem(EloSettings(32.0, 1000.0))
+
+    // Add one round of results which will all use the same "original" ratings - this might be a tournament.
+    // The result is from the perspective of the first team or player, and is simply a float in the range 
+    // 0.0 .. 1.0, where 1.0 is a win.
+    let results1 = seq {
+        yield Result("Russia", "New Zealand", BasicResult.Win)   // Russia beat New Zealand
+        yield Result("Portugal", "Mexico", BasicResult.Draw)     // Brazil and Afghanistan draw
+        yield Result("Cameroon", "Chile", BasicResult.Loss)      // Cameron lose to Chile
+        yield Result("Australia", "Germany", BasicResult.Loss)   // Australia lose to Germany
+        yield Result("Russia", "Portugal", BasicResult.Loss)     // Russia lose to Portgual
+        yield Result("Mexico", "New Zealand", BasicResult.Win) } // Mexico beat New Zealand
+
+    for team in rankings.Rankings do printfn "%A" team
+
+This will output the below:
+
+    Portugal: 1016
+    Mexico: 1016
+    Chile: 1016
+    Germany: 1016
+    Russia: 1000
+    Cameroon: 984
+    Australia: 984
+    New Zealand: 968
